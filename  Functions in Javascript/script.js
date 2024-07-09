c4aa199310fc41a190863cdb05dadea723161ed0
RamiHadji <43167480+RamiHadji@users.noreply.github.com>
2024-07-08 22:16:12 -0400

    Add files via upload

---
+++  Functions in Javascript/script.js
@@ -0,0 +1,9 @@
+function printWelcomeMessage(name){
+    console.log("Hello " + name)
+    console.log("Welcome to js ")
+    console.log("How are you?")
+
+}
+printWelcomeMessage("Nadia")
+printWelcomeMessage("Dahdouh")
+printWelcomeMessage("Mouna")
