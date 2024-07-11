000f1e9f817c7d631670b148a7451878cd926f05
RamiHadji <43167480+RamiHadji@users.noreply.github.com>
2024-07-11 14:24:57 -0400

    Add files via upload

---
+++  Functions With Returned Values/script.js
@@ -0,0 +1,14 @@
+function numberSquaerd(number1=19, number2=40){
+    
+    var result = number1 * number2
+
+return result    
+}
+console.log("the result is: " + numberSquaerd()) 
+console.log("the Nembur is: " + numberSquaerd()) 
+console.log(numberSquaerd()) 
+
+
+var arr = [20, 10, 50 ,80]
+arr.reverse()
+console.log(arr.reverse())
