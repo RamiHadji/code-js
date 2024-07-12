69da3d39267aaf020cfd972215cae732047d13d0
RamiHadji <43167480+RamiHadji@users.noreply.github.com>
2024-07-12 09:06:47 -0400

    Add files via upload

---
+++ 39: Arrow Functions In Javascript/Callback/script.js
@@ -0,0 +1,13 @@
+function test(x,c,d,f){
+    console.log("hi")
+}
+
+var x = test
+//var y = function(){
+    //console.log("hi form y")
+//}
+
+var y = () => {
+    console.log("hi from y")
+}
+y()
