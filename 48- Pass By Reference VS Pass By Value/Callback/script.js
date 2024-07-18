fe055897726715ff6feef25a1f652c902b52ffa8
RamiHadji <43167480+RamiHadji@users.noreply.github.com>
2024-07-18 09:33:20 -0400

    Add files via upload

---
+++ 48- Pass By Reference VS Pass By Value/Callback/script.js
@@ -0,0 +1,13 @@
+// let x = 'hi'
+// let y = x
+
+// x = 'Tarmeez'
+
+let x = {
+     name: 'Khaled'
+}
+let y = x
+
+x.name = 'Dived'
+console.log('The x value is: ' , x)
+console.log('The x value is: ' , y)
