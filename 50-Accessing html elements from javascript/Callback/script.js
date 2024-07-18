25fa8336712fd9a9a5412df1aa40a8442dea23f7
RamiHadji <43167480+RamiHadji@users.noreply.github.com>
2024-07-18 10:26:43 -0400

    Add files via upload

---
+++ 50-Accessing html elements from javascript/Callback/script.js
@@ -0,0 +1,16 @@
+//document.getElementById("title").innerHtml =  'Welcom To Tarmeez Channel';
+
+// let elm1 = document.getElementById("title")
+// let elm2 = document.getElementsByClassName("title")[1].innerHTML = 'ramon'
+
+// let x = document.getElementsByTagName("h3")
+// document.getElementsByTagName("h3")[0].innerHTML = "hi mam"
+
+// console.log(elm1)
+// console.log(elm2)
+// console.log(x)
+
+for (elment of document.getElementsByTagName("h3")){
+
+     elment.innerHTML = "hi"
+}
